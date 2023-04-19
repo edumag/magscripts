0b504849b6a95969825719ed7dbc4ee7653611f7
edumag <edu@lesolivex.com>
2023-04-19 13:34:55 +0200

    añadimos sudo

--- sistema/update.sh
+++ sistema/update.sh
@@ -5,37 +5,37 @@ echo "pkcon"
 echo "-----"
 echo "pkcon refresh force -y"
 echo
-pkcon refresh force -y
+sudo pkcon refresh force -y
 echo
 echo "pkcon get-update"
 echo
-pkcon get-updates
+sudo pkcon get-updates
 echo
 echo "pkcon update"
 echo
-pkcon update -y
+sudo pkcon update -y
 
 echo
 echo "flatpak"
 echo "-------"
 echo "flatpak list"
 echo
-flatpak list
+sudo flatpak list
 echo
 echo "flatpak -v  update"
 echo
-flatpak update
+sudo flatpak update
 
 echo
 echo "snap"
 echo "----"
 echo "snap list"
 echo
-snap list
+sudo snap list
 echo
 echo "snap refresh"
 echo
-snap refresh
+sudo snap refresh
 
 echo
 echo "Limpieza"
