8d257367059ee9e2088ac5147aca9a08198a7b43
edumag <edu@lesolivex.com>
2024-08-21 08:17:57 +0200

    Controlando salida en sistema/software-update.sh

--- sistema/software-search.sh
+++ sistema/software-search.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-which apt-get > /dev/null
+which apt-get > /dev/null 2>&1
 if [[ $? -eq 0 ]] ; then
   echo "apt-get | sudo apt-get install <package>"
   echo "-------"
   apt-cache search $@
 fi
 
-which pacman > /dev/null
+which pacman > /dev/null 2>&1
 if [[ $? -eq 0 ]] ; then
   echo "pacman | sudo pacman -S <package>"
   echo "------"
   pacman -Ss $@
 fi
 
-which snap > /dev/null
+which snap > /dev/null 2>&1
 if [[ $? -eq 0 ]] ; then
   echo
   echo "snap | sudo snap install <package>"
@@ -22,7 +22,7 @@ if [[ $? -eq 0 ]] ; then
   snap search $@
 fi
 
-which flatpak > /dev/null
+which flatpak > /dev/null 2>&1
 if [[ $? -eq 0 ]] ; then
   echo
   echo "flatpak | sudo flatpak install <package>"
